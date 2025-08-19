6073981427ad88ab62285a07cb83173eb17cb7cb
Scott Daniel <scottdaniel@mac.mynetworksettings.com>
2025-08-18 21:07:51 -0400

    Original working app

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import { enableScreens } from 'react-native-screens';
 import AppContainer from './src/navigations/AppNavigation';
+
+enableScreens(true);
 
 export default function App() {
   return (
